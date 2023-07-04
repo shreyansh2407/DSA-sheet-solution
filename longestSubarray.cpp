65d340fa78d6c0c5e7025740494d3b4e92c801d6
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-07-05 00:12:30 +0530

    Longest sub array

---
+++ longestSubarray.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+//brute force solution
+int longest(vector<int> &arr,int n,int target){
+    vector<int> temp={0};
+   int len =0;
+    for(int i=0;i<n;i++){
+         int sum=0;
+        for(int j = i;j<n;j++){
+           sum+=arr[j];
+           if(sum==target){
+            len = max(len,j-i+1);
+           }
+           
+        }
+    }
+    
+    return len;
+}
+
+//better solution
+int main() {
+    vector<int> arr  = {1,2,3,4,5,6};
+    int target=6;
+    int n = arr.size();
+    int ans = longest(arr,n,target);
+    cout<<ans;
+      return 0;
+}
